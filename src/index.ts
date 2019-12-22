3f43e164f13c28a43d0a05644aa7754c20e8c83c
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2019-12-22 23:03:36 +0100

    Added complete sentry logging for errors;

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,25 @@ import express from 'express';
 import Sentry from '@sentry/node';
 const {PORT = 3000, SENTRY_REPORTING = false, SENTRY_DSN = ''} = process.env;
 
+/* istanbul ignore next */
 if(SENTRY_REPORTING) {
     Sentry.init({ dsn: SENTRY_DSN});
 }
 
 export const app = express();
 
+/* istanbul ignore next */
+if(SENTRY_REPORTING) {
+    app.use(Sentry.Handlers.requestHandler());
+}
+
 app.route('/').get((req, res) => res.send('Welcome to shokz.tv-v2 API').end());
+
+
+/* istanbul ignore next */
+if(SENTRY_REPORTING) {
+    app.use(Sentry.Handlers.errorHandler());
+}
 
 app.listen(PORT);
 console.log('REST Server started on: ' + PORT + ', with' + (!SENTRY_REPORTING ? 'out' : '') + ' error reporting.');
