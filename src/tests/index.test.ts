493d383ba5a080d815a29fb9f113765aa64155f1
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2019-12-22 22:36:14 +0100

    using force exit for jest tests;

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,14 @@
 import request from 'supertest';
 import {app} from './../index';
 
+afterAll(async () => {
+	await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+});
+
 describe('Test the root path', () => {
-    test('It should response the GET method', async () => {
+    test('It should response the GET method', async (done) => {
         await request(app).get('/').expect(200);
+
+        done();
     });
 })
