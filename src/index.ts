86693594424e786c77a3e543e02e91c660536e5f
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2019-12-22 19:27:01 +0100

    Removed reporter from express integration of sentry; Fixed shutdown;

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,8 @@ if(SENTRY_REPORTING) {
 
 const app = express();
 
-if(SENTRY_REPORTING) {
-    app.use(Sentry.Handlers.requestHandler());
-}
-
 app.route('/')
    .get((req, res) => res.send('Welcome to shokz.tv-v2 API'));
-
-if(SENTRY_REPORTING) {
-    app.use(Sentry.Handlers.errorHandler());
-}
 
 app.listen(PORT);
 console.log('REST Server started on: ' + PORT + ', with' + (!SENTRY_REPORTING ? 'out' : '') + ' error reporting.');
