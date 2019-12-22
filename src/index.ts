5f1c9340d4508427656e4a2ad7621dd4c458fadd
Michael Ketzer <michael@ketzer.email>
2019-12-22 14:31:45 +0100

    Started express basic app, added sentry reporting;

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import express from 'express';
+import dotenv from 'dotenv';
+import Sentry from '@sentry/node';
+dotenv.config();
+const {PORT = 3000, SENTRY_REPORTING = false, SENTRY_DSN = ''} = process.env;
+
+if(SENTRY_REPORTING) {
+    Sentry.init({ dsn: SENTRY_DSN});
+}
+
+const app = express();
+
+if(SENTRY_REPORTING) {
+    app.use(Sentry.Handlers.requestHandler());
+}
+
+//Route definitions
+
+if(SENTRY_REPORTING) {
+    app.use(Sentry.Handlers.errorHandler());
+}
+
+app.listen(PORT);
+console.log('REST Server started on: ' + PORT + ', with' + (!SENTRY_REPORTING ? 'out' : '') + ' error reporting.');
