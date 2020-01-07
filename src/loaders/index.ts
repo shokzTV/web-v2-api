3da326907c2bc825ea429edb82717dc7c40d28ab
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-01-07 20:43:44 +0100

    Clean up

--- src/loaders/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import expressLoader from './express';
-
-export default async ({ app }: {app: express.Application}) => {
-    await expressLoader({ app });
-    console.info('✌️ Express loaded');
-};
