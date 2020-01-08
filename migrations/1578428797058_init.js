8b679f6e3f1c0ba72aac9c82380157571bdcf0cb
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-01-08 20:48:12 +0100

    Added missing auto increment; Added default rights;

--- migrations/1578428797058_init.js
+++ migrations/1578428797058_init.js
@@ -1,14 +1,14 @@
 module.exports = {
     "up": (conn, cb) => {
         conn.query(`
-            CREATE TABLE tag (id int NOT NULL, name varchar(191) NOT NULL, image varchar(191) NOT NULL, PRIMARY KEY (id));
-            CREATE TABLE role (id int NOT NULL, name varchar(191) NOT NULL, PRIMARY KEY (id));
-            CREATE TABLE \`right\` (id int NOT NULL, name  varchar(191) NOT NULL, ident varchar(191) NOT NULL, PRIMARY KEY (id));
+            CREATE TABLE tag (id int NOT NULL AUTO_INCREMENT, name varchar(191) NOT NULL, image varchar(191) NOT NULL, PRIMARY KEY (id));
+            CREATE TABLE role (id int NOT NULL AUTO_INCREMENT, name varchar(191) NOT NULL, PRIMARY KEY (id));
+            CREATE TABLE \`right\` (id int NOT NULL AUTO_INCREMENT, name  varchar(191) NOT NULL, ident varchar(191) NOT NULL, PRIMARY KEY (id));
             CREATE TABLE user (id int NOT NULL, twitch_id int NOT NULL, display_name varchar(191) NOT NULL, avatar varchar(191) NOT NULL, profile_url varchar(191) NOT NULL, custom_title varchar(191) NOT NULL, PRIMARY KEY (id));
             CREATE TABLE role_rights (right_id int NOT NULL, role_id int NOT NULL, PRIMARY KEY (right_id, role_id), KEY fkIdx_17 (role_id), CONSTRAINT FK_17 FOREIGN KEY fkIdx_17 (role_id) REFERENCES role (id), KEY fkIdx_22 (right_id), CONSTRAINT FK_22 FOREIGN KEY fkIdx_22 (right_id) REFERENCES \`right\` (id));
             CREATE TABLE user_roles (user_id int NOT NULL, role_id int NOT NULL, PRIMARY KEY (user_id, role_id), KEY fkIdx_21 (user_id), CONSTRAINT FK_21 FOREIGN KEY fkIdx_21 (user_id) REFERENCES user (id), KEY fkIdx_26 (role_id), CONSTRAINT FK_26 FOREIGN KEY fkIdx_26 (role_id) REFERENCES role (id));
-            CREATE TABLE article (id int NOT NULL, title varchar(191) NOT NULL, author int NOT NULL, created datetime NOT NULL, cover varchar(191) NOT NULL, body longtext NOT NULL, status enum('draft', 'published', 'hidden') NOT NULL, PRIMARY KEY (id), KEY fkIdx_73 (author), CONSTRAINT FK_73 FOREIGN KEY fkIdx_73 (author) REFERENCES user (id));
-            CREATE TABLE media (id integer NOT NULL, article_id int NOT NULL, type enum('source', 'video', 'article') NOT NULL, reference varchar(191) NOT NULL, reference_source varchar(191) NOT NULL, PRIMARY KEY (id), KEY fkIdx_50 (article_id), CONSTRAINT FK_50 FOREIGN KEY fkIdx_50 (article_id) REFERENCES article (id));
+            CREATE TABLE article (id int NOT NULL AUTO_INCREMENT, title varchar(191) NOT NULL, author int NOT NULL, created datetime NOT NULL, cover varchar(191) NOT NULL, body longtext NOT NULL, status enum('draft', 'published', 'hidden') NOT NULL, PRIMARY KEY (id), KEY fkIdx_73 (author), CONSTRAINT FK_73 FOREIGN KEY fkIdx_73 (author) REFERENCES user (id));
+            CREATE TABLE media (id integer NOT NULL AUTO_INCREMENT, article_id int NOT NULL, type enum('source', 'video', 'article') NOT NULL, reference varchar(191) NOT NULL, reference_source varchar(191) NOT NULL, PRIMARY KEY (id), KEY fkIdx_50 (article_id), CONSTRAINT FK_50 FOREIGN KEY fkIdx_50 (article_id) REFERENCES article (id));
             CREATE TABLE article_tags (article_id int NOT NULL, tag_id int NOT NULL, PRIMARY KEY (article_id, tag_id), KEY fkIdx_14 (tag_id), CONSTRAINT FK_14 FOREIGN KEY fkIdx_14 (tag_id) REFERENCES tag (id), KEY fkIdx_37 (article_id), CONSTRAINT FK_37 FOREIGN KEY fkIdx_37 (article_id) REFERENCES article (id));
         `, (err) => {
             if(err) {
