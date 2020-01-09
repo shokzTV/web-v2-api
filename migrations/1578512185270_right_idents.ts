93f5886c0d9fd1dc37cdb2cf6ee14ba5807d9a49
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-01-09 17:32:06 +0100

    Moved everything to typescript;

--- migrations/1578512185270_right_idents.ts
+++ migrations/1578512185270_right_idents.ts
@@ -1,4 +1,4 @@
 module.exports = {
     "up": "INSERT INTO `right` (name, ident) VALUES ('Add articles', 'ARTICLE_CREATE'), ('Edit articles', 'ARTICLE_EDIT'), ('Delete articles', 'ARTICLE_DELETE'), ('Publish articles', 'ARTICLE_PUBLISH'), ('Unpublish articles', 'ARTICLE_UNPUBLISH'), ('Add guide', 'GUIDE_CREATE'), ('Edit guide', 'GUIDE_EDIT'), ('Delete guide', 'GUIDE_DELETE'), ('Admin access', 'ADMIN_ACCESS');",
-    "down": "DELETE FROM `right`;"
+    "down": "DELETE FROM `role_rights`;DELETE FROM `right`;"
 }
