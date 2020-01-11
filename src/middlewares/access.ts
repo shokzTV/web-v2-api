7587575a8bc2a91e5ad1db6bc752e434bcdc4d4e
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-01-11 22:15:16 +0100

    Fixed wrong status for unauthorized requests;

--- src/middlewares/access.ts
+++ src/middlewares/access.ts
@@ -3,11 +3,11 @@ import { User } from "../entities/User";
 
 export const checkUserRole = (roleIdent: string) => (req: Request, res: Response, next: NextFunction) => {
     if(!req.user) {
-        return res.json({msg: 'Unauthorized'}).status(401).end();
+        return res.status(401).json('unauthorized').end();
     }
 
     if(! (req.user as User).roles.find(({rights}) => rights?.find(({ident}) => ident === roleIdent))) {
-        return res.json({msg: 'Forbidden'}).status(403).end();
+        return res.status(403).json('forbidden').end();
     }
 
     return next();
