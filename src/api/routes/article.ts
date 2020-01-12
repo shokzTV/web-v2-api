bbb097f9bbc6f4a15ee4e474f2437460e2da0fa3
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-01-12 21:26:15 +0100

    Added API to add articles ad modify basic stuff on articles;

---
+++ src/api/routes/article.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response } from 'express';
+import { checkUserRole } from '../../middlewares/access';
+import { UploadedFile } from 'express-fileupload';
+import { User } from '../../entities/User';
+import { createDraft, publishArticle, unpublishArticle, assignTag, removeTag } from '../../services/article';
+
+const route = Router();
+
+export default (app: Router) => {
+    app.use('/article', route);
+
+    route.post('/create', checkUserRole('ARTICLE_CREATE'), async (req: Request, res: Response) => {
+        const userId = (req.user! as User).id;
+        const cover = req.files && req.files.cover ? req.files.cover as UploadedFile : null;
+        const {title, body, tags} = req.body;
+        const articleId = await createDraft(title, body, tags, userId, cover);
+        return res.json(articleId).status(201);
+    });
+
+    route.patch('/:articleId/publish', checkUserRole('ARTICLE_PUBLISH'), async (req: Request, res: Response) => {
+        await publishArticle(+req.params.articleId);
+        return res.json().status(204);
+    });
+
+    route.patch('/:articleId/unpublish', checkUserRole('ARTICLE_UNPUBLISH'), async (req: Request, res: Response) => {
+        await unpublishArticle(+req.params.articleId);
+        return res.json().status(204);
+    });
+
+    route.put('/:articleId/tag', checkUserRole('ARTICLE_EDIT'), async (req: Request, res: Response) => {
+        await assignTag(+req.params.articleId, req.body.tag);
+        return res.json().status(204);
+    });
+
+    route.delete('/:articleId/tag/:tagId', checkUserRole('ARTICLE_EDIT'), async (req: Request, res: Response) => {
+        await removeTag(+req.params.articleId, +req.params.tagId);
+        return res.json().status(204);
+    });
+};
