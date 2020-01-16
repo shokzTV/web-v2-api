76cfe84bcb31674b62718b0f638d84a8ab26eec9
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-01-16 23:02:33 +0100

    Added image resize and saving;

--- src/services/article.ts
+++ src/services/article.ts
@@ -2,7 +2,8 @@ import { UploadedFile } from 'express-fileupload';
 import { getConn } from '../db';
 import uuid from 'uuid/v5';
 import { OkPacket, RowDataPacket } from 'mysql2';
-import {requireTags, Tag} from './tags';
+import {requireTags, Tag} from './tag';
+import sharp from 'sharp';
 
 enum Status {
     draft = 'draft',
@@ -10,7 +11,7 @@ enum Status {
     hidden = 'hidden',
 }
 
-interface Article extends RowDataPacket {
+interface Article {
     id: number;
     title: string;
     body: string;
@@ -21,8 +22,28 @@ interface Article extends RowDataPacket {
     }>;
     cover: string;
     status: Status;
-    author: number;
+    author: {
+        id: number,
+        twitch: number,
+        avatar: string;
+        name: string;
+        title: string;
+    };
     created: number;
+}
+
+interface ArticleRow extends RowDataPacket {
+    articleId: number;
+    title: string;
+    body: string;
+    cover: string;
+    status: Status;
+    created: number;
+    userId: number;
+    twitch_id: number;
+    display_name: string;
+    avatar: string;
+    custom_title: string;
 }
 
 interface TagResponse extends Tag {
@@ -39,13 +60,25 @@ export async function getArticles(articleIds: number[] = []): Promise<Article[]>
         params.push(articleIds);
     }
 
-    const [articles] = await conn.execute<Article[]>(`SELECT id, title, author, body, cover, status, FROM_UNIXTIME(created) as created FROM article ${condition}`, params);
+    const [articles] = await conn.execute<ArticleRow[]>(`SELECT a.id as articleId, a.title, a.body, a.cover, a.status, FROM_UNIXTIME(a.created) as created, u.id as userId, u.twitch_id, u.display_name, u.avatar, u.custom_title FROM article a INNER JOIN user u ON u.id = a.author ${condition}`, params);
     const [tags] = await conn.execute<TagResponse[]>(`SELECT at.article_id as article, t.id, t.name, t.image FROM article_tags at INNER JOIN tag t ON t.id = at.tag_id ${condition}`, params);
 
     await conn.end();
 
     return articles.map((a) => ({
-        ...a,
+        id: a.articleId,
+        title: a.title,
+        body: a.body,
+        cover: a.cover,
+        status: a.status,
+        created: a.created,
+        author: {
+            id: a.userId,
+            twitch: a.twitch_id,
+            name: a.display_name,
+            avatar: a.avatar,
+            title: a.custom_title
+        },
         tags: tags.filter(({article}) => article === a.id).map(({id, name, image}) => ({id, name, image})),
     }));
 }
@@ -56,9 +89,15 @@ export async function createDraft(title: string, body: string, tags: string[], u
     let imagePath: string= '';
     if(cover) {
         const imgHash = uuid(title, uuid.URL);
-        imagePath = `/static/covers/${imgHash}.jpg`;
-        const fullPath = __dirname + `/../..${imagePath}`;
-        cover.mv(fullPath);
+        imagePath = `/static/covers/${imgHash}.webp`;
+        const orgImagePath = `/static/covers/${imgHash}_orig.webp`;
+        await sharp(cover.data)
+            .resize({ width: 512, height: 288 })
+            .webp({ lossless: true })
+            .toFile(__dirname + `/../..${imagePath}`);
+        await sharp(cover.data)
+            .webp({ lossless: true })
+            .toFile(__dirname + `/../..${orgImagePath}`);
     }
 
     const [{insertId}] = await conn.execute<OkPacket>(
@@ -89,7 +128,6 @@ async function assignTags(articleId: number, tags: string[]): Promise<void> {
     const conn = await getConn();
     
     for(const tag of tags) {
-        console.log(articleId, tagMap, tag);
         await conn.execute('INSERT INTO article_tags (article_id, tag_id) VALUES (?, ?);', [articleId, tagMap[tag]]);
     }
     await conn.end();
