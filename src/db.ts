48a1ca094d5715d60b3b644654def0e8b948c0c7
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-02-02 19:57:08 +0100

    Added last action date for a tag;

--- src/db.ts
+++ src/db.ts
@@ -4,5 +4,6 @@ import { Connection } from 'mysql2/promise';
 
 export async function getConn(): Promise<Connection> {
     const conn = await mysql.createConnection(config.mysql);
+    await conn.execute(`SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))`);
     return conn;
 }
