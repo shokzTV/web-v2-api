e73c4408136e9376b8c80b6b219a21cbf7135be2
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-02-09 19:40:09 +0100

    Fixed missing auto increment

--- migrations/1581249261634_videos_table.js
+++ migrations/1581249261634_videos_table.js
@@ -1,7 +1,7 @@
 module.exports = {
     "up": (conn, cb) => {
         conn.query(`
-            CREATE TABLE video (id int NOT NULL, source text NOT NULL, title varchar(191) NOT NULL, description varchar(191) NOT NULL, thumbnail varchar(191) NOT NULL, PRIMARY KEY (id));
+            CREATE TABLE video (id int NOT NULL AUTO_INCREMENT, source text NOT NULL, title varchar(191) NOT NULL, description varchar(191) NOT NULL, thumbnail varchar(191) NOT NULL, PRIMARY KEY (id));
             CREATE TABLE video_tags (tag_id int NOT NULL, video_id int NOT NULL, PRIMARY KEY (tag_id, video_id), KEY fkIdx_45 (tag_id), CONSTRAINT FK_45 FOREIGN KEY fkIdx_45 (tag_id) REFERENCES tag (id), KEY fkIdx_51 (video_id), CONSTRAINT FK_51 FOREIGN KEY fkIdx_51 (video_id) REFERENCES video (id));
             INSERT INTO \`right\` (name, ident) VALUES ('Add videos', 'VIDEO_CREATE'), ('Edit videos', 'VIDEO_EDIT'), ('Delete videos', 'VIDEO_DELETE');
         `, (err) => {
