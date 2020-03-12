a2986cd2d21a85c395ba6c59cffed79312b97084
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-12 22:24:15 +0100

    Added fetch route for current app version;

--- src/api/routes/base.ts
+++ src/api/routes/base.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import fetch from 'node-fetch';
 const route = Router();
 
 export default (app: Router) => {
@@ -6,5 +7,11 @@ export default (app: Router) => {
 
   route.get('/', (req: Request, res: Response) => {
     return res.json({ msg: 'Welcome to shokz.tv api' }).status(200);
+  });
+
+  route.get('/version', async (req: Request, res: Response) => {
+    const response = await fetch('https://gitlab.com/api/v4/projects/16127808/repository/commits/master');
+    const {id} = await response.json();
+    return res.json(id).status(200);
   });
 };
