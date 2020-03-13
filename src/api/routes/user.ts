d50e7d14792bc2151a37fa04c8cab786f46216d4
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-13 22:38:38 +0100

    Added missing rights check;

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,16 +1,17 @@
 import { Router, Request, Response } from 'express';
 import { loadUsers, updateUserRole } from '../../services/user';
+import { checkUserRole } from '../../middlewares/access';
 const route = Router();
 
 export default (app: Router) => {
   app.use('/user', route);
 
-  route.get('/list', async (req: Request, res: Response) => {
+  route.get('/list', checkUserRole('ADMIN_ACCESS'), async (req: Request, res: Response) => {
       const users = await loadUsers();
       return res.json(users).status(200);
   });
 
-  route.patch('/role/:userId', async (req: Request, res: Response) => {
+  route.patch('/role/:userId', checkUserRole('ADMIN_ACCESS'), async (req: Request, res: Response) => {
     await updateUserRole(+req.params.userId, +req.body.roleId);
     return res.send().status(204);
   });
