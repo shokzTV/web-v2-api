9c0f002dc441d2e5d9936d3a91e96241fa292fca
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-03-17 22:10:27 +0100

    Added sitemap xml generator;

--- src/api/routes/base.ts
+++ src/api/routes/base.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import fetch from 'node-fetch';
+import { createSitemap } from '../../services/sitemap';
 const route = Router();
 
 export default (app: Router) => {
@@ -7,6 +8,11 @@ export default (app: Router) => {
 
   route.get('/', (req: Request, res: Response) => {
     return res.json({ msg: 'Welcome to shokz.tv api' }).status(200);
+  });
+
+  route.get('/sitemap', async (req: Request, res: Response) => {
+    const xml = await createSitemap();
+    return res.type('application/xml').send(xml).status(200);
   });
 
   route.get('/version', async (req: Request, res: Response) => {
